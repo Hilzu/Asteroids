bea146c8cb7b0617a5a61403eedf33b8ea28c3e5
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2011-08-21 02:56:42 +0300

    Removed unused import

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,6 @@
 
 import java.nio.FloatBuffer;
 import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector2f;
