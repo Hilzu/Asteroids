f8a3c46898b9ebcf98bfd5cac3dfc01a9b141571
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2011-08-21 03:19:41 +0300

    Removed netbeans template comments.

--- src/Movable.java
+++ src/Movable.java
@@ -1,12 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Hilzu
- */
 interface Movable extends Drawable {
     
     void move(float angle, float distance);    
